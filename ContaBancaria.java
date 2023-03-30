c60ab40adc84814ad7be9b97f8dae279c884df40
Matheus Gomes <115513930+MatheusGomes0@users.noreply.github.com>
2023-03-30 17:36:13 -0300

    Add files via upload

--- ContaBancaria.java
+++ ContaBancaria.java
@@ -12,42 +12,62 @@ public class ContaBancaria {
 
 	//metodo construtor = valor padrao para todos os objetos 
 	public ContaBancaria() {
-		this.status = false;
-		this.saldo = 0;
+		this.setStatus(false);
+		this.setSaldo(0);
 	}
 	//metodos 
 	public void abrirConta() {
-		this.status = true;
-		if(this.tipo == "CC") {
+		this.setStatus(true);
+		if(this.getTipo() == "CC") {
 			System.out.println("Sua conta corrente foi aberta com sucesso");
-			this.saldo = 50;
+			this.setSaldo(50); //quando abrir em conta corrente a pessoa ganha 50
 		} else {
 			System.out.println("Sua conta poupança foi aberta com sucesso");
-			this.saldo = 150;
+			this.setSaldo(150); // quando abrir em poupança a pessoa ganha 150
 		}
 	}
 
 	public void fecharConta() {
-		if(this.saldo == 0) {
-			this.status = false;
+		if(this.getSaldo() == 0) {
+			this.setStatus(false);
 			System.out.println("Sua conta foi fechada");
-		} else if (this.saldo>0) {
+		} else if (this.getSaldo()>0) {
 			System.out.println("Voce ainda possui dinheiro na conta, saque primeiro para fechar");
 		} else {
 			System.out.println("Sua conta esta com saldo negativo, ela não pode ser fechada");
 		}
 	}
 
-	public void depositar() {
+	//outra forma de ser feito sem a entrada gráfica
+	public void depositar(float num) {
+		if(this.isStatus() == true) {
+			setSaldo(this.saldo += num);
+		} else {
+			System.out.println("Sua conta esta fechada");
+		}
+	}
+
+	/*public void depositar() {
 		if(this.status == true) {
 			float num = Float.parseFloat(JOptionPane.showInputDialog("Qual o valor que deseja depositar?"));
 			this.saldo += num;
 		} else {
 			System.out.println("Sua conta esta fechada");
 		}
-	}
+	}*/
 
-	public void sacar() {
+	public void sacar(float num) {
+		if(this.isStatus() == true) {
+			if(this.getSaldo()>0) {
+				setSaldo(this.saldo -= num);
+			} else {
+				System.out.println("Sua conta não possui dinheiro para sacar");
+			}
+		} else if(this.isStatus() == false) {
+			System.out.println("Sua conta está fechada");
+		}
+	}
+	/*public void sacar() {
 		if(this.status == true) {
 			if(this.saldo>0) {
 				float num = Float.parseFloat(JOptionPane.showInputDialog("Qual o valor que deseja sacar?"));
@@ -58,13 +78,13 @@ public class ContaBancaria {
 		} else {
 			System.out.println("Sua conta está fechada");
 		}
-	}
+	}*/
 
 	public void pagarMensal() {
-		if(this.tipo == "CC") {
-			this.saldo -= 12;
+		if(this.getTipo() == "CC") {
+			this.setSaldo(saldo -= 12);
 		} else {
-			this.saldo -=20;
+			this.setSaldo(saldo -= 20);
 		}
 	}
 
@@ -102,15 +122,15 @@ public class ContaBancaria {
 
 
 	//metodo para mostrar p resultado
-	    public void mostrar() {
+	public void mostrar() {
 		System.out.println("O saldo da sua conta é: " + this.getSaldo());
 		System.out.println("Sua conta está aberta? " + this.isStatus());
-	    }
-		public void informacoes() {
-			System.out.println("Informações sobre sua conta");
-			System.out.println("O número da sua conta é: " + this.getNumConta());
-			System.out.println("O tipo da sua conta é: " + this.getTipo());
-			System.out.println("Seu nome é: " + this.getNomeDono());
-		}
-	
+	}
+	public void informacoes() {
+		System.out.println("Informações sobre sua conta");
+		System.out.println("O número da sua conta é: " + this.getNumConta());
+		System.out.println("O tipo da sua conta é: " + this.getTipo());
+		System.out.println("Seu nome é: " + this.getNomeDono());
+	}
+
 }
